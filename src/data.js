db07ab09a34c378ccfa667accdbec0ab29120bef
TheVaultboy11 <thevaultboy11@gmail.com>
2023-07-14 12:34:27 +0200

    adding the blogs and the fixed navbar

--- src/data.js
+++ src/data.js
@@ -2,8 +2,10 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
-   
-   
+    {
+      text: 'Home',
+      href: getPermalink(),
+    },
     {
       text: 'Blog',
       href: getBlogPermalink(),
@@ -71,7 +73,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    
+    Made by <a class="text-blue-600 hover:underline dark:text-gray-200"> Starshipshield</a> · All rights reserved.
   `,
 };
